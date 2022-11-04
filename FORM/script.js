e227ce045180158e1665f817789bb07608dc101f
JulioX <115498048+Julioxli@users.noreply.github.com>
2022-11-03 21:50:58 -0300

    Add files via upload

---
+++ FORM/script.js
@@ -0,0 +1,6 @@
+var botaoEnviar = document.getElementById('enviar');
+function changeColor(){
+    botaoEnviar.style.color = 'green';
+}
+
+var field = document.getElementById('campo').style.backgroundColor = 'purple';
